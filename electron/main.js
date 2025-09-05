1ba5108a9c81c356544cdb61836b1e7e29b828e6
I-hashimi04 <ibrarhashimi1@gmail.com>
2025-09-05 11:15:53 +0100

    Add Electron main process

--- electron/main.js
+++ electron/main.js
@@ -1,15 +1,16 @@
-// Electron main process bootstrap
 const { app, BrowserWindow } = require('electron');
 
-function createWindow() {
+function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: __dirname + '/preload.js'
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
-  win.loadURL('http://localhost:3000'); // Dev: React server, will be file:// in prod
+
+  win.loadURL('http://localhost:3000'); // React UI dev server
 }
 
 app.whenReady().then(createWindow);
